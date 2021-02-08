13cbf26cdca02ca40c54c9e93652fc270e23a574
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2021-02-08 12:26:56 +0100

    Coroutine 2

---
+++ Courotine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Courotine : MonoBehaviour
+{
+    public float time = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Coroutine()
+    {
+
+        Debug.LogWarning("Ik start nu de coroutine");
+        //yield return new WaitForSeconds(time);
+
+        for (int i = 0; i < 11; i++)
+        {
+            Debug.Log("coroutine update" + i);
+        }
+        
+        yield return new WaitForSeconds(time);
+        Debug.LogWarning("coroutine einde");
+    }
+}
