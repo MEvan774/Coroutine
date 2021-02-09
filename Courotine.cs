9811852281779da870d4acd3882595be1a1f7ee0
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2021-02-09 10:17:03 +0100

    Added fadeCoroutine

--- Courotine.cs
+++ Courotine.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class Courotine : MonoBehaviour
 {
     public float time = 0.5f;
+    public Renderer renderer;
+    //public Material material;
 
     // Start is called before the first frame update
     void Start()
     {
+        //renderer = GetComponent<Renderer>();
+        //material = GetComponent<Renderer>().material;
         StartCoroutine(Coroutine());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     IEnumerator Coroutine()
@@ -24,12 +22,27 @@ public class Courotine : MonoBehaviour
         Debug.LogWarning("Ik start nu de coroutine");
         //yield return new WaitForSeconds(time);
 
-        for (int i = 0; i < 11; i++)
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
         {
-            Debug.Log("coroutine update" + i);
+
+
+            Color c = renderer.material.color;
+
+            //Color c = new Color(1,1,1,ft);
+            c.a = ft;
+
+            renderer.material.color = c;
+            //renderer.material.SetColor("Color", c);
+            //material.SetColor("color", c);
+
+
+            //renderer.material.color = c;
+
+            yield return new WaitForSeconds(time);
+            //yield return null;
         }
         
-        yield return new WaitForSeconds(time);
+
         Debug.LogWarning("coroutine einde");
     }
 }
